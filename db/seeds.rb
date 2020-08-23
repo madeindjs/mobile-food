63c93df6dfef4469c2e6e136265561c322360c52
Blaise Popote <blaisepopote@gmail.com>
2020-08-23 20:39:21 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 50 fake partners...'
+50.times do
+  partner = Partner.new(
+    food_type: Faker::Restaurant.type,
+    company_name: Faker::Restaurant.name,
+    first_name: Faker::Name.first_name,
+    last_name:Faker::Name.last_name,
+    address:Faker::Address.city,
+    phone_number:Faker::PhoneNumber.cell_phone_with_country_code,
+    email:Faker::Internet.email,
+    picture_url:"https://images.unsplash.com/photo-1467278941343-c6d371b23a90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80",
+    description: Faker::Restaurant.description,
+    min_guest:rand(10..100),
+    comment:Faker::Restaurant.review,
+    review:rand(0..5)
+  )
+  partner.save!
+end
+puts 'Finished!'
+
+
+puts 'Creating 50 fake clients...'
+50.times do
+  client = Client.new(
+  first_name: Faker::Name.first_name,
+  last_name:Faker::Name.last_name,
+  company: Faker::Restaurant.name,
+  email:Faker::Internet.email,
+  phone_number:Faker::PhoneNumber.cell_phone_with_country_code,
+  comment:Faker::Quote.famous_last_words,
+  status: "new"
+  )
+  client.save!
+end
+puts 'Finished!'
