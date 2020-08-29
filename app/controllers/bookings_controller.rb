1bfa28648610700a2fae165dd13e73aa0cb344f6
Blaise Popote <blaisepopote@gmail.com>
2020-08-29 11:19:56 +0200

    booking start

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,52 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only:[:show, :destroy, :edit, :update]
+
   def new
+    @booking = Booking.new
+  end
+
+  def index
+    @bookings_grid = initialize_grid(Booking,
+      order:           'bookings.date',
+      order_direction: 'desc'
+      )
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save!
+      redirect_to clients_path
+    else
+      flash[:alert] = "Booking rejected"
+      render 'new'
+    end
   end
+
+  def show
+    @client = @booking.client_id
+  end
+
+  def edit
+
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
+  end
+
+   def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :city, :client_id, :guest_quantity, :food_type, :has_electricity, :chosen_partner_id, :requested_quotes_partners)
+  end
+
 end
+
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
+  has_many :bookings
   validates :first_name, :last_name, :email, :status, presence: true
 
 end
