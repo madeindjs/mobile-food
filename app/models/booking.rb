b013bd61eadce5707bc9414b45aa0eeabcf9e982
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-09-06 17:43:26 +0200

    Oops

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,16 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :client
   validates :city, :date, presence: true
-
-  before_save :split_food_type
-
-  private
-
-  def split_food_type
-    puts self.inspect
-    puts food_type.inspect
-    puts @food_type.inspect
-    puts self.food_type.inspect
-    # if @food_type
-  end
 end
